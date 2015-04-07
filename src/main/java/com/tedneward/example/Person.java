6c8b209aaba3a42a47b68f03a5be8905fc4c12ef
ykim253 <ykim253@uw.edu>
2015-04-07 15:38:46 -0700

    i modified and tested what was asked

--- src/main/java/com/tedneward/example/Person.java
+++ src/main/java/com/tedneward/example/Person.java
@@ -3,12 +3,13 @@ package com.tedneward.example;
 import java.beans.*;
 import java.util.*;
 
-public class Person {
+public class Person implments Comparable<Person> {
   private int age;
   private String name;
   private double salary;
   private String ssn;
   private boolean propertyChangeFired = false;
+  private static int count;
   
   public Person() {
     this("", 0, 0.0d);
@@ -18,18 +19,42 @@ public class Person {
     name = n;
     age = a;
     salary = s;
+    ssn = "";
+    count++;
   }
 
   public int getAge() {
     return age;
   }
+
+  public void setAge(int age) {
+    if (age < 0) {
+      throw new IllegalArgumentException;
+    }
+    this.age = age;
+  }
   
   public String getName() {
     return name;
   }
+
+  public void setName(String name) {
+    if (name == null) {
+      throw new IllegalArgumentException;
+    }
+    this.name = name;
+  }
+
+  public int count() {
+    return count;
+  }
   
   public double getSalary() {
     return salary;
+  }
+
+  public void setSalary(double salary) {
+    this.salary = value;
   }
   
   public String getSSN() {
@@ -46,6 +71,10 @@ public class Person {
     return propertyChangeFired;
   }
 
+  public void setPropertyChangeFired(boolean propertyChangeFired) {
+    this.propertyChangeFired = propertyChangeFired;
+  } 
+
   public double calculateBonus() {
     return salary * 1.10;
   }
@@ -59,11 +88,43 @@ public class Person {
   }
   
   public boolean equals(Person other) {
-    return (this.name.equals(p.name) && this.age == p.age);
+    if (other instanceof Person) {
+      Person o = (Person) other;
+      return this.name.equals(o.getName()) && this.age == o.getAge();
+    } else {
+      return false;
+    }
   }
 
-  public String tostring() {
-    return "{{FIXME}}";
+  public static class AgeComparator implements Comparator<Person>{
+    public int compare(Person p1,Person p2) {
+      return p1.getAge() - p2.getAge();
+    }
+  }
+
+  @Override
+  public int compareTo(Person o) {
+    if (this.getSalary() > o.getSalary()) {
+      return -1;
+    } else if (this.getSalary() < o.getSalary()) {
+      return  1;
+    } else {
+      return 0;
+    }
+  }
+
+  @Override
+  public String toString() {
+    return ("[Person name:" + this.name + " age:" + this.age + " salary:" + this.salary + "]");
+  }
+
+  public static ArrayList<Person> getNewardFamily() {
+    ArrayList<Person> fam = new ArrayList<Person>();
+    fam.add(new Person("Ted",41,250000.));
+    fam.add(new Person("Charlotte",43,150000.));
+    fam.add(new Person("Michael",22,10000.));
+    fam.add(new Person("Matthew",15,0.));
+    return fam;
   }
 
   // PropertyChangeListener support; you shouldn't need to change any of
--- src/test/java/com/tedneward/example/TestPerson.java
+++ src/test/java/com/tedneward/example/TestPerson.java
@@ -91,16 +91,13 @@ public class TestPerson {
   public void catchPropertyChange() {
     Person ted = new Person("Ted", 43, 250000);
 
-    // ============ YOUR CHANGES BEGIN HERE
-    // Call addPropertyChangeListener with a PropertyChangedListener
-    // that has the following code in it:
-    /*
-    assertEquals("ssn", pce.getPropertyName());
-    assertEquals("", pce.getOldValue());
-    assertEquals("012-34-5678", pce.getNewValue());
-    */
-
-    // ============ YOUR CHANGES END HERE
+    ted.addPropertyChangeListener(new PropertyChangeListener() {
+      public void propertyChange(PropertyChangeEvent pce) {
+        assertEquals("ssn", pce.getPropertyName());
+        assertEquals("", pce.getOldValue());
+        assertEquals("012-34-5678", pce.getNewValue());
+      }
+    });
     
     assertEquals(false, ted.getPropertyChangeFired());
     ted.setSSN("012-34-5678");
